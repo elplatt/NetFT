1c8d287017888613a41bffd51ff7663dcd409abf
Edward L Platt <ed@elplatt.com>
2017-10-15 16:31:04 -0400

    Update bare python .

--- Router.py
+++ Router.py
@@ -12,6 +12,7 @@ import time
 from multiprocessing import Process, Queue
 import networkx as nx
 from networkx.algorithms.components import connected_components
+from networkx.algorithms.connectivity import node_connectivity
 from networkx.algorithms.centrality.betweenness import betweenness_centrality
 from networkx.algorithms.distance_measures import diameter
 import numpy as np
@@ -22,8 +23,10 @@ import logbook
 
 # In[ ]:
 
-rewire_f = 0.1
+rewire_f = 0.9
+#rewire_f = sys.argv[1]
 butterfly_m = 7
+num_conn_pairs = 150
 net_file = "external/as20000102.csv"
 out_file = "stats.csv"
 try:
@@ -99,17 +102,21 @@ node_count = len(nodes)
 
 # In[ ]:
 
-def do_centrality_work(g):
+def do_failure_work(g):
     centralities = betweenness_centrality(g, normalized=False)
     v, c = max(centralities.iteritems(), key=lambda x: x[1])
     return v, c
 
-def centrality_worker(graph_q, component_inq, centrality_outq):
+def failure_worker(graph_q, component_inq, connectivity0_inq, connectivity1_inq, connectivity2_inq, connectivity3_inq, failure_outq):
     while True:
         g = graph_q.get()
         component_inq.put(g)
-        v, c = do_centrality_work(g)
-        centrality_outq.put((v,c))
+        connectivity0_inq.put(g)
+        connectivity1_inq.put(g)
+        connectivity2_inq.put(g)
+        connectivity3_inq.put(g)
+        v, c = do_failure_work(g)
+        failure_outq.put((v,c))
         if c > 0:
             next_g = g.copy()
             next_g.remove_node(v)
@@ -166,13 +173,97 @@ def size_worker(size_inq, size_outq):
 
 # In[ ]:
 
-graph_q = Queue()
-centrality_outq = Queue()
-component_inq = Queue()
-diameter_inq = Queue()
+def sample_pairs(nodes, num):
+    # Sample sources and targets
+    sources = random.sample(nodes, num)
+    targets = random.sample(nodes, num)
+    # Replace self loops
+    for i in range(num - 1):
+        if sources[i] == targets[i]:
+            sources[i], sources[-1] = sources[-1], sources[i]
+    if sources[-1] == targets[-1]:
+        sources[-1] = random.choice(set(nodes) - set(sources))
+    return zip(sources, targets)
+    
+def expand(g, source, radius):
+    if radius == 0:
+        return set(), set()
+    # Get all nodes within radius
+    neighbors = set(nx.ego_graph(g, source, radius=radius).nodes())
+    neighbors.remove(source)
+    # Keep track of the boundary and remove nodes within radius
+    boundary = set()
+    for v in neighbors:
+        boundary |= set(nx.neighbors(g, v))
+    # Remove source (unless boundary is empty)
+    boundary.discard(source)
+    return neighbors, boundary
+        
+def do_connectivity_work(g, radius):
+    pairs = sample_pairs(g.nodes(), num_conn_pairs)
+    # Calculate connectivity for sampled pairs
+    connectivities = []
+    for source, target in pairs:
+        s_neighbors, s_boundary = expand(g, source, radius)
+        t_neighbors, t_boundary = expand(g, target, radius)
+        expanded = g.copy()
+        # Get list of all neighbors, remaining s/t boundaries
+        neighbors = (t_neighbors | s_neighbors) - set([target]) - set([source])
+        s_boundary -= neighbors
+        t_boundary -= neighbors
+        # If neighborhoods overlap, combine
+        common = s_neighbors & t_neighbors
+        if len(common) > 0:
+            boundary = (s_boundary | t_boundary) - set([target]) - set([source])
+            s_boundary = boundary
+            t_boundary = boundary
+            target = source
+        # Remove neighbors and create edges to boundary
+        for v in s_boundary:
+            expanded.add_edge(source, v)
+        for v in t_boundary:
+            expanded.add_edge(target, v)
+        for v in neighbors:
+            expanded.remove_node(v)
+        try:
+            c = node_connectivity(expanded, source, target)
+        except nx.NetworkXError:
+            print source, target
+            print s_boundary
+            print t_boundary
+            print neighbors
+            raise
+        connectivities.append(c)
+    # Calculate statistics
+    mean = np.mean(connectivities)
+    std = np.std(connectivities, ddof=1)
+    se = mean / np.sqrt(len(connectivities))
+    return (mean, se)
+
+def connectivity_worker(connectivity_inq, connectivity_outq, radius):
+    while True:
+        g = connectivity_inq.get()
+        data = do_connectivity_work(g, radius)
+        connectivity_outq.put(data)
+
+
+# In[ ]:
+
+graph_q = Queue(maxsize=4)
+failure_outq = Queue()
+component_inq = Queue(maxsize=4)
+diameter_inq = Queue(maxsize=4)
 diameter_outq = Queue()
-size_inq = Queue()
+size_inq = Queue(maxsize=4)
 size_outq = Queue()
+connectivity0_inq = Queue(maxsize=4)
+connectivity0_outq = Queue()
+connectivity1_inq = Queue(maxsize=4)
+connectivity1_outq = Queue()
+connectivity2_inq = Queue(maxsize=4)
+connectivity2_outq = Queue()
+connectivity3_inq = Queue(maxsize=4)
+connectivity3_outq = Queue()
 
 exp = logbook.Experiment(exp_name, suffix=exp_suffix)
 log = exp.get_logger()
@@ -184,10 +275,14 @@ graph_q.put(g)
 
 log.info("Starting workers")
 workers = []
-workers.append(Process(target=centrality_worker, args=(graph_q, component_inq, centrality_outq)))
+workers.append(Process(target=failure_worker, args=(graph_q, component_inq, connectivity0_inq, connectivity1_inq, connectivity2_inq, connectivity3_inq, failure_outq)))
 workers.append(Process(target=component_worker, args=(component_inq, diameter_inq, size_inq)))
 workers.append(Process(target=diameter_worker, args=(diameter_inq, diameter_outq)))
 workers.append(Process(target=size_worker, args=(size_inq, size_outq)))
+workers.append(Process(target=connectivity_worker, args=(connectivity0_inq, connectivity0_outq, 0)))
+workers.append(Process(target=connectivity_worker, args=(connectivity1_inq, connectivity1_outq, 1)))
+workers.append(Process(target=connectivity_worker, args=(connectivity2_inq, connectivity2_outq, 2)))
+workers.append(Process(target=connectivity_worker, args=(connectivity3_inq, connectivity3_outq, 3)))
 
 for w in workers:
     w.daemon = True
@@ -196,17 +291,25 @@ for w in workers:
 with open(exp.get_filename(out_file), "wb") as out:
     log.info("Starting")
     finished = 0
-    out.write("removed,diameter,size,failed,high_betweenness,node_count,rewire_f,butterfly_m\n")
+    out.write("removed,diameter,size,node_conn0_mean,node_conn0_se,node_conn1_mean,node_conn1_se,node_conn2_mean,node_conn2_se,node_conn3_mean,node_conn3_se,node_conn_pairs,failed,high_betweenness,node_count,rewire_f,butterfly_m,failure_type\n")
     while finished < node_count:
         log.info("Iteration {}".format(finished))
         log.info("  Finding betweenness")
-        label, centrality = centrality_outq.get()
+        label, centrality = failure_outq.get()
         log.info("  Finding diameter")
         diameter = diameter_outq.get()
         log.info("  Finding size")
         size = size_outq.get()
+        log.info("  Finding 0-connectivity")
+        node_conn0_mean, node_conn0_se = connectivity0_outq.get()
+        log.info("  Finding 1-connectivity")
+        node_conn1_mean, node_conn1_se = connectivity1_outq.get()
+        log.info("  Finding 2-connectivity")
+        node_conn2_mean, node_conn2_se = connectivity2_outq.get()
+        log.info("  Finding 3-connectivity")
+        node_conn3_mean, node_conn3_se = connectivity3_outq.get()
         log.info("  Writing row")
-        row = [finished, diameter, size, label, centrality, node_count, rewire_f, butterfly_m]
+        row = [finished, diameter, size, node_conn0_mean, node_conn0_se, node_conn1_mean, node_conn1_se, node_conn2_mean, node_conn2_se, node_conn3_mean, node_conn3_se, num_conn_pairs, label, centrality, node_count, rewire_f, butterfly_m,"targeted"]
         out.write(",".join([str(d) for d in row]) + "\n")
         out.flush()
         finished += 1
